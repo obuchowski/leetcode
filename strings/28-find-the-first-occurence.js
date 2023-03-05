e25d0f18443e3abdd1e5f4b8f6fb7cc8ddfac143
Evgeni Obukhovski <eobuhovsky@gmail.com>
2023-03-05 10:49:47 +0100

    Mar 03, 2023: #28

---
+++ strings/28-find-the-first-occurence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        for (let j = 0; j < needle.length; j++) {
+            if (needle[j] !== haystack[j + i]) {
+                break
+            }
+            if (j === needle.length - 1) {
+                return i
+            }
+        }
+    }
+
+    return -1
+}
+
+console.log(strStr('aabaaabaaac', 'aabaaac'))
