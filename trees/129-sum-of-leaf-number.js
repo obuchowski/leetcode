e37f4bfa5b3500946847c8db9b14168bd013f42c
Evgeni Obukhovski <eobuhovsky@gmail.com>
2023-03-15 10:47:31 +0100

    Mar 14, 2023: #129

---
+++ trees/129-sum-of-leaf-number.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root, val = 0) {
+    if (null === root) {
+        return 0
+    }
+
+    const sum = val * 10 + root.val
+    if (null === root.left && null === root.right) {
+        return sum
+    }
+
+    return sumNumbers(root.left, sum) + sumNumbers(root.right, sum)
+}
+
+console.log(sumNumbers(new TreeNode(1, new TreeNode(0),new TreeNode(0))))
